6734890f75dffe3bdc8675cd19809c101742bc6c
Ale <alejandro.berutti@convorelay.com>
2026-03-19 15:20:52 -0300

    URSA-7727 | Updating flutter_webrtc version

--- lib/src/rtc_session.dart
+++ lib/src/rtc_session.dart
@@ -701,7 +701,11 @@ class RTCSession extends EventManager implements Owner {
       if (_status == C.STATUS_TERMINATED) {
         return;
       }
-      logger.e('Failed to answer(): ${error.toString()}', error, s);
+      logger.e(
+        'Failed to answer(): ${error.toString()}',
+        error: error,
+        stackTrace: s,
+      );
     }
   }
 
@@ -1774,7 +1778,7 @@ class RTCSession extends EventManager implements Owner {
       _setLocalMediaStatus();
       _iceRestart();
     } catch (error, stacktrace) {
-      logger.e(error.toString(), null, stacktrace);
+      logger.e(error.toString(), stackTrace: stacktrace);
       if (_isIceRestartRetryWindowActive) {
         _scheduleIceRestart();
       }
@@ -2532,7 +2536,7 @@ class RTCSession extends EventManager implements Owner {
 
       request_sender.send();
     } catch (error, s) {
-      logger.e(error.toString(), null, s);
+      logger.e(error.toString(), stackTrace: s);
       _failed('local', null, null, null, 500, DartSIP_C.CausesType.WEBRTC_ERROR,
           'Can\'t create local SDP');
       if (_status == C.STATUS_TERMINATED) {
@@ -2807,7 +2811,7 @@ class RTCSession extends EventManager implements Owner {
         'eventHandlers': handlers
       });
     } catch (e, s) {
-      logger.e(e.toString(), null, s);
+      logger.e(e.toString(), stackTrace: s);
       onFailed();
     }
   }
