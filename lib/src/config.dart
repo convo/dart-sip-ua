78e6ccda06ba690641b381b966d7076cd39db98d
Felipe <felipe.medeiros@convorelay.com>
2026-03-30 18:37:56 -0300

    Fix hardening SDP

--- lib/src/config.dart
+++ lib/src/config.dart
@@ -62,7 +62,7 @@ class Settings {
   String? hostport_params;
 
   /// ICE Gathering Timeout (in millisecond).
-  int ice_gathering_timeout = 500;
+  int ice_gathering_timeout = 1000;
 
   /// Sip Message Delay (in millisecond) ( default 0 ).
   int sip_message_delay = 0;
