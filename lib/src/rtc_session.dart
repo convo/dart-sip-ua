90cdf8edd2f61d00d327beed40ad7a6ccdac2b0f
Felipe <felipe.medeiros@convorelay.com>
2026-04-22 17:58:01 -0400

    URSA-7859 | Finish call on 481 BYW

--- lib/src/rtc_session.dart
+++ lib/src/rtc_session.dart
@@ -1184,6 +1184,7 @@ class RTCSession extends EventManager implements Owner {
 
     handlers.on(EventCallFailed(), (EventCallFailed event) {
       logger.d('renegotiate EventCallFailed');
+
       if (_isIceConnectionRetrying) {
         if (_isIceRestartRetryWindowActive) {
           _scheduleIceRestart();
@@ -1483,8 +1484,16 @@ class RTCSession extends EventManager implements Owner {
     }
   }
 
-  void onDialogError([bool isRenegotiating = false]) {
+  void onDialogError([bool isRenegotiating = false, dynamic response]) {
     logger.e('onDialogError()');
+
+    if (response is IncomingResponse && response.status_code == 481) {
+      terminate(<String, dynamic>{
+        'cause': DartSIP_C.CausesType.CALL_DOES_NOT_EXIST,
+        'status_code': response.status_code,
+        'reason_phrase': DartSIP_C.REASON_PHRASE[response.status_code]
+      });
+    }
 
     if (isCallRecoverable(isRenegotiating)) return;
 
@@ -2924,6 +2933,14 @@ class RTCSession extends EventManager implements Owner {
 
     void onFailed([dynamic response]) {
       eventHandlers.emit(EventCallFailed(session: this, response: response));
+
+      if (response is IncomingResponse && response.status_code == 481) {
+        terminate(<String, dynamic>{
+          'cause': DartSIP_C.CausesType.CALL_DOES_NOT_EXIST,
+          'status_code': response.status_code,
+          'reason_phrase': DartSIP_C.REASON_PHRASE[response.status_code]
+        });
+      }
     }
 
     void onSucceeded(IncomingResponse? response) async {
@@ -3003,7 +3020,7 @@ class RTCSession extends EventManager implements Owner {
       });
 
       handlers.on(EventOnDialogError(), (EventOnDialogError event) {
-        onDialogError(isRenegotiating); // Do nothing because session ends.
+        onDialogError(isRenegotiating, event.response);
       });
 
       sendRequest(SipMethod.INVITE, <String, dynamic>{
